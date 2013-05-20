1b87fb6c938e449b826ea75127767e55c702e0e5
TonySeek <tonyseek@gmail.com>
2013-05-20 14:37:30 +0800

    use new chrome extension api.

--- scripts/autoauth.js
+++ scripts/autoauth.js
@@ -8,15 +8,11 @@ $(function(){
     $("object[classid='clsid:305C213C-780C-432D-8417-23E53F2EE830']").each(function(){
         var id = $(this).attr("id");
 
-        try {
-            $("<input>").attr("type", "password")
-                .attr({"disabled": "disabled",
-                       "width": $(this).attr("width")})
-                .val("szuauth")
-                .appendTo($(this));
-        } catch (e) {
-            console.log(e.stack);
-        }
+        $("<input>").attr("type", "password")
+            .attr({"disabled": "disabled",
+                   "width": $(this).attr("width")})
+            .val("szuauth")
+            .appendTo($(this));
 
         chromex("getHashPassword", [], function(result){
             var script = document.createElement("script");
